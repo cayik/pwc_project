132a63552cdf556455eea32f68bbb510432a91ec
cayik <cayan.ayik1@gmail.com>
2023-12-24 19:27:25 +0100

    Docker hinzugefügt

--- Models/database/WoWDbContext.cs
+++ Models/database/WoWDbContext.cs
@@ -25,22 +25,18 @@ namespace pwc_project.Models.database
         public DbSet<EquipmentSlot> EquipmentSlot { get; set; }
 
 
-
-
-
         public WoWDbContext(IConfiguration configuration)
         {
             _configuration = configuration;
 
         }
-
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString = $"Server={_configuration["SERVER_IP"]};" +
                                         $"User ID={_configuration["USER_NAME"]};" +
                                         $"Password={_configuration["USER_PASSWORD"]};" +
                                         $"Database={_configuration["DATABASE_NAME"]}";
+
             optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         }
 
--- Program.cs
+++ Program.cs
@@ -6,26 +6,29 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Füge Dienste zum Container hinzu.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Erfahre mehr über die Konfiguration von Swagger/OpenAPI unter https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
+// Lade Konfigurationsdatei für Geheimnisse, optional und mit automatischem Neuladen bei Änderungen.
 builder.Configuration.AddJsonFile("secrets.json", optional: true, reloadOnChange: true);
 
-
+// Konfiguriere SwaggerGen mit OAuth2-Sicherheitsdefinitionen.
 builder.Services.AddSwaggerGen(options =>
 {
-    options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme {
+    options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
         In = Microsoft.OpenApi.Models.ParameterLocation.Header,
         Name = "Authorization",
         Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey
     });
 
+    // Füge Filter für Sicherheitsanforderungen hinzu.
     options.OperationFilter<SecurityRequirementsOperationFilter>();
-
 });
+
+// Füge JWT-Authentifizierung hinzu und konfiguriere Validierungsoptionen.
 builder.Services.AddAuthentication().AddJwtBearer(
     options =>
     {
@@ -35,27 +38,25 @@ builder.Services.AddAuthentication().AddJwtBearer(
             ValidateAudience = false,
             ValidateIssuer = false,
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT_SECRET"]))
-
         };
-
-
     }
-    
-    
-    );
-
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Konfiguriere den HTTP-Anforderungs-Pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Benutze Swagger in der Entwicklungsphase.
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Benutze die Autorisierungsmiddleware.
 app.UseAuthorization();
 
+// Kartiere die Controller-Endpunkte.
 app.MapControllers();
 
+// Starte die Anwendung.
 app.Run();
